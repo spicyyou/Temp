643ddf1bc3dc1f24c7c94a6734680eb4e22734db
spicyyou <hyj5419@naver.com>
2022-06-27 22:44:33 +0900

    Initial Commit

---
+++ src/main/java/com/developeronwheels/pictoyou/PictoyouApplication.java
@@ -0,0 +1,13 @@
+package com.developeronwheels.pictoyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictoyouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PictoyouApplication.class, args);
+	}
+
+}
